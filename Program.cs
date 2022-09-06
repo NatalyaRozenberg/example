566ce2661f06d2ea64ea36f5975a0b864c0ec0e5
Natalya Rozenberg <rozdeg25@gmail.com>
2022-09-06 17:56:51 +1000

    Добавлн файл Програм

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+ 
+string[] array =
+{
+    "I",
+    "see",
+    "the",
+    "sea",
+    "1250",
+    "5000"
+};
+ 
+var result = new string[array.Length];
+var newSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[newSize] = value;
+        newSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, newSize));
